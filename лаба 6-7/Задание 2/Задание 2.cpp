06edc32c2e75bcebbc2396db4fd57d05e4ddbf7b
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-11-09 17:52:18 +0300

    Выполнила задание 2

---
+++ лаба 6-7/Задание 2/Задание 2.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>              //ввод библиотек
+#include <math.h>
+ 
+
+int main()                 //начало функции, начало программы
+{
+	int i, b, mass[] = { 24,35,65,6,0,9,3 }, c;                // объявление переменных и массива
+	int n = sizeof(mass) / sizeof(int);                 // размера массива
+	const float k = 1.247;                      //объявление константы
+	c = n - 1;
+
+
+	while (c >= 1){                //начало цикла
+		i = 0;
+		while (i < n - c){
+			if (mass[i] > mass[i + c]){
+				b = mass[i];
+				mass[i] = mass[i + c];
+				mass[i + c] = b;
+			}
+			i++; 
+		}
+		c /= k; 
+	}
+	for (i = 0; i < n; i++){
+		printf("%d ", mass[i]); // вывод измененного массива
+	}
+	return 0;
+}

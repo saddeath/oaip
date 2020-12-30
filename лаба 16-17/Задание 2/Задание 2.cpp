24813d0da7321a8759234491fd6cb81f7d056978
ada_c <ada_c@LAPTOP-PEB0OI70>
2020-12-30 13:47:42 +0300

    Задание 2 выполнила

---
+++ лаба 16-17/Задание 2/Задание 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>       // ввод библиотеки 
+
+
+void qsort(int* mass, int a, int b)       // начало функции и программы 
+{
+
+	if (a < b){
+		int ml, c = a, x = b, m = mass[(a + b) / 2];           // объявление переменных
+		for (c; c <= x; c){
+			while (mass[c] < m){                 // цикл, если значение меньше опорного
+				c++;
+			}
+			while (mass[x] > m){           // цикл, если значение больше опорного
+				x--;
+			}
+			if (c <= x){
+				int ml = mass[c];
+				mass[c] = mass[x];
+			    mass[x] = ml;
+				c++;
+				x--;
+			}
+		}
+		while (c <= x);               // проверка 
+		qsort(mass, a, x);
+		qsort(mass, c, b);
+	}
+}
+
+
+int main()
+{
+
+	int y;
+	int mass[] = { 13,25,5,8,0,9,17};           // объявление массива
+	int n = sizeof(mass) / sizeof(int);         // размер массива
+	qsort(mass, 0, n - 1);                     // вызов функции сортировки
+	for (y = 0; y < n; y++){
+		printf_s("%i ", mass[y]);            // вывод измененного масива
+	}
+}
